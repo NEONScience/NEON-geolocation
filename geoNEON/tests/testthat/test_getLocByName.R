d2cf5697ff079dcb9b870536c7a9d8fa5c4e41dd
Claire Lunch <clunch@battelleecology.org>
2020-02-04 14:36:10 -0700

    update testing

--- geoNEON/tests/testthat/test_getLocByName.R
+++ geoNEON/tests/testthat/test_getLocByName.R
@@ -1,17 +1,18 @@
-context("def.extr.geo tests")
+context("Test getLocByName")
+library(geoNEON)
 
 test_that("GUAN_044.basePlot.ltr location values are correct", {
   df<-data.frame(namedLocation=c("GUAN_044.basePlot.ltr", "GUAN_045.basePlot.ltr"), otherData=c(1,2))
-  out<-def.extr.geo.os(df)
+  out<-getLocByName(df)
   out<-out[order(out$namedLocation),]
   expect_equal(out$api.description, c('Plot \"GUAN_044\" at site \"GUAN\"', 'Plot \"GUAN_045\" at site \"GUAN\"'))
   expect_equal(as.numeric(out$api.northing), c(1987798.71019852, 1988068.47644692))
 })
 
 
-test_that("error messages are correct", {
+test_that("Nonsense inputs correctly return warning message", {
   df<-data.frame(namedLocation=c("nonsense", "nonsense"), otherData=c(1,2))
-  expect_warning(def.extr.geo.os(df),'WARNING: the following namedLocation was not found: nonsense')
+  expect_warning(getLocByName(df),'WARNING: the following namedLocation was not found: nonsense')
 })
 
 
--- geoNEON/tests/testthat/test_getLocTOS.R
+++ geoNEON/tests/testthat/test_getLocTOS.R
@@ -1,19 +1,19 @@
-context("def.calc.geo.os")
+context("Test getLocTOS for each data product type")
+library(geoNEON)
 
 test_that("Select bird named locations are correct", {
   df<-data.frame(namedLocation=c('SCBI_023.basePlot.brd', 'BART_003.birdGrid.brd', 
                                 'BART_003.birdGrid.brd', 'BART_003.birdGrid.brd'), pointID=c(21,'A1', 'B2', 'B2'),
                  pointCountMinute=c(1,2,3,88))
-  out<-def.calc.geo.os(df, 'brd_countdata')
-  #out<-out[order(row.names(out),]
-  expect_equal(as.numeric(out$northing), c(4308018.7592, 4880715.9042, 4880465.9323, 4881510.4750), tolerance=1)
+  out<-getLocTOS(df, 'brd_countdata')
+  expect_equal(as.numeric(out$northing), c(4308017.759165, 4880714.9041744, 4880464.93234, 4881510.4750), tolerance=0.1)
   expect_equal(as.numeric(out$adjCoordinateUncertainty), c(0.24, 15, 0.32, NA), tolerance=0.01)
 })
 
 test_that("Select smammal named locations are correct", {
   df<-data.frame(namedLocation=c('BART_001.mammalGrid.mam', 'BART_001.mammalGrid.mam',
                                  'BART_084.mammalGrid.mam'), trapCoordinate=c('J9','X1', 'E5'))
-  out<-def.calc.geo.os(df, 'mam_pertrapnight')
+  out<-getLocTOS(df, 'mam_pertrapnight')
   expect_equal(as.numeric(out$northing), c(4879827.13675762, NA, 4879827.1367), tolerance=1)
   expect_equal(as.numeric(out$adjCoordinateUncertainty), c(NA, NA, 1.18), tolerance=0.01)
 })
@@ -21,7 +21,7 @@ test_that("Select smammal named locations are correct", {
 test_that("Select plant pres named locations are correct", {
   df<-data.frame(namedLocation=c('BART_036.basePlot.div', 'BART_071.basePlot.div',
                                  'BART_019.basePlot.div'), subplotID=c('32.4.1','32.4.10', '32'))
-  out<-def.calc.geo.os(df, 'div_10m2Data100m2Data')
+  out<-getLocTOS(df, 'div_10m2Data100m2Data')
   expect_equal(as.numeric(out$adjNorthing), c(4881309.50371794, 4881968.47614891, 4882112.72882359), tolerance=1)
   expect_equal(as.numeric(out$adjCoordinateUncertainty), c(NA, NA, 2.29), tolerance=0.01)
 })
