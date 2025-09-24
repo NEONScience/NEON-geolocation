1b2eae8166904d190d0c97f62737aa79bf51bc78
hschartel <hschartel@battelleecology.org>
2025-09-24 11:24:44 -0600

    Update getLocProperties.R
    test null headers

--- geoNEON/R/getLocProperties.R
+++ geoNEON/R/getLocProperties.R
@@ -9,9 +9,9 @@
 #'
 #' @param locList A list derived from the JSON object returned by the locations endpoint of the NEON API.
 #' @param history Does locJSON include the location history? T or F, defaults to F.
-#' 
+#'
 #' @keywords internal
-#' 
+#'
 #' @return A data frame of location data.
 
 #' @references
@@ -23,39 +23,59 @@
 ##############################################################################################
 
 getLocProperties <- function(locList, history=FALSE) {
-  
+
+  #test
+  .null2na <- function(x) {
+    if (is.list(x)) return(lapply(x, .null2na))
+    if (is.null(x)) return(NA)
+    x
+  }
+  #
+
   if(!history) {
-    loc.values <- locList$data[intersect(intersect(grep('ParentUrl', names(locList$data), invert=T), 
+    loc.values <- locList$data[intersect(intersect(grep('ParentUrl', names(locList$data), invert=T),
                                                    grep('Child', names(locList$data), invert=T)),
                                          grep('Properties', names(locList$data), invert=T))]
-    
+
     loc.props.mat <- locList$data$locationProperties
     loc.props <- unlist(lapply(loc.props.mat, function(x) {
       x$locationPropertyValue
     }))
-    names(loc.props) <- unlist(lapply(loc.props.mat, function(x) { 
+
+    names(loc.props) <- unlist(lapply(loc.props.mat, function(x) {
       x$locationPropertyName
     }))
-    
+
     loc.all <- unlist(c(loc.values, loc.props), use.names=T)
     loc.all <- data.frame(t(loc.all))
   }
-  
+
   if(history) {
     loc.values <- locList[grep('Properties', names(locList), invert=T)]
+    #test
+    loc.values <- lapply(loc.values, .null2na)
+    loc.values <- unlist(loc.values, use.names = TRUE)
+    #
 
     loc.props.mat <- locList$locationProperties
     loc.props <- unlist(lapply(loc.props.mat, function(x) {
-      x$locationPropertyValue
+      #x$locationPropertyValue
+    #}))
+      #test
+      if (is.null(x$locationPropertyValue)) NA else x$locationPropertyValue
+    }), use.names = FALSE)
+    #
+    names(loc.props) <- unlist(lapply(loc.props.mat, function(x) {
+      #x$locationPropertyName
+    #}))
+      #test
+      if (is.null(x$locationPropertyName)) NA_character_ else x$locationPropertyName
     }))
-    names(loc.props) <- unlist(lapply(loc.props.mat, function(x) { 
-      x$locationPropertyName
-    }))
-    
+
     loc.all <- unlist(c(loc.values, loc.props), use.names=T)
     loc.all <- data.frame(t(loc.all))
   }
-  
+
   return(loc.all)
-  
+
 }
